fa9a6eb4e825e1176688888919993bd424b4d4d0
Adin <Adin@DESKTOP-4B57U2Q>
2017-03-28 10:04:25 +0200

    Add project files.

---
+++ DAS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DAS.Startup))]
+namespace DAS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
