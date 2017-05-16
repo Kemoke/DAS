1c55e212faba0212bd23c92a4b40b7f190fe520c
Adin <cebic@gmail.com>
2017-05-16 21:03:36 +0200

    Additional comments to indicate work distribution

--- DAS/Controllers/ManageController.cs
+++ DAS/Controllers/ManageController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// Created by Adin Cebic
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
--- DAS/Models/DocumentModels1.cs
+++ DAS/Models/DocumentModels1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// Created by Adin Cebic
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
